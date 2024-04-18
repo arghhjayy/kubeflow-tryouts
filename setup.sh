bd4f80a26e97e7bf943a0c4a76090da21679c62a
Rupanshu Janbandhu <arghhjayy@gmail.com>
2024-04-18 20:33:21 +0530

    add_setup_file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+brew install kind
+kind create cluster
+
+# from: https://www.kubeflow.org/docs/components/pipelines/v1/installation/localcluster-deployment/#deploying-kubeflow-pipelines
+export PIPELINE_VERSION=2.1.0
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
+kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=$PIPELINE_VERSION"
